5e793b1f2870c8f754f7eb67dd580bc4abe632c5
DanielsSon12 <danielouverney67@gmail.com>
2023-09-28 22:15:24 -0300

    JSLab

--- JS/Laboratório Script/script.js
+++ JS/Laboratório Script/script.js
@@ -17,17 +17,110 @@ const valorTotal = calcularValorTotalEstoque(produtos);
 document.querySelector('#produtoResposta').innerHTML = "Valor total do estoque: "+valorTotal;
 
 //ATIVIDADE 2
-const buscarPrevisaoTempo = async cidade => {
-    const api = await fetch('https://github.com/adinan-cenci/climatempo-api');
-}
-
-buscarPrevisaoTempo('São Paulo').then((dados) => {
-    document.querySelector('#resultado').innerHTML = "Dados da previsão do tempo: "+dados;
-})
-.catch((erro) => {
-    HTMLFormControlsCollection.error('Erro ao buscar previsão do tempo:'+erro);
-});
+let buscarPrevisaoTempo = document.querySelector('#buscar');
+let InputCidade = document.querySelector('#cidade');
 
 document.addEventListener('DOMContentLoaded', function(){
-    //Código aqui
+    const buscarAPI = async cidade => {
+        //Implementação aqui usando a Fetch API
+        const api = await fetch('https://apiadvisor.climatempo.com.br/api/v1/locale/city?name=${cidade}');
+    }
+    
+    buscarPrevisaoTempo.addEventListener("click", function() {
+        const cidade = InputCidade.value;
+        if(cidade.trim() == ""){
+            alert("Digite o nome da cidade!");
+            return;
+        }
+    
+        fetch(buscarAPI(cidade))
+            .then((response) => {
+                if(!response.ok){
+                    throw new Error (`Houve um erro na requisição: ${response.status}`);
+                }
+                return response.json
+            })
+            .then((data) => {
+                const temperatura = data.data.temperature;
+                const condicoes = data.data.condition;
+
+                document.querySelector('#resultado').innerHTML = `
+                    <h2>Previsão do tempo para ${cidade}</h2>
+                    <p>Temperatura: ${temperatura} C°</p>
+                    <p>Condições: ${condicoes}</p>
+                `;
+            })
+            .catch((error) => {
+                console.error("Erro ao tentar obter a previsão do tempo: ", error);
+                document.querySelector('#resultado').innerHTML = `Erro ao tentar buscar a previsão do tempo.`;
+            })
+    })
 });
+
+//ATIDADE 3
+async function enviarComentario(comentario) {
+
+    // Sua implementação aqui usando AJAX
+    
+    }
+    
+    enviarComentario('Este é um ótimo artigo!')
+    
+    .then(() => {
+    
+    console.log('Comentário enviado com sucesso.');
+    
+    })
+    
+    .catch((erro) => {
+    
+    console.error('Erro ao enviar comentário:', erro);
+    
+    });
+
+/*
+async function enviarComentario(comentario) {
+  // Construa os parâmetros da solicitação AJAX, dependendo da sua estrutura de servidor
+  const parametros = {
+    method: 'POST', // Ou 'PUT', 'PATCH', etc., dependendo da sua API
+    headers: {
+      'Content-Type': 'application/json', // Defina o tipo de conteúdo apropriado
+    },
+    body: JSON.stringify(comentario), // Converta o objeto de comentário em JSON
+  };
+
+  // Substitua a URL abaixo pela URL do endpoint da API do servidor de comentários
+  const url = 'https://sua-api.com/comentarios';
+
+  try {
+    const response = await fetch(url, parametros);
+
+    if (!response.ok) {
+      // Se a resposta não for bem-sucedida, rejeite a Promise com uma mensagem de erro
+      throw new Error(`Erro ao enviar o comentário: ${response.status}`);
+    }
+
+    // Aguarde a resposta ser processada e retorne uma mensagem de sucesso
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    // Se ocorrer um erro durante a solicitação, rejeite a Promise com o erro
+    throw error;
+  }
+}
+
+// Exemplo de uso da função
+const novoComentario = {
+  autor: 'Nome do Autor',
+  texto: 'Este é um novo comentário.',
+};
+
+enviarComentario(novoComentario)
+  .then((resposta) => {
+    console.log('Comentário enviado com sucesso:', resposta);
+  })
+  .catch((erro) => {
+    console.error('Erro ao enviar o comentário:', erro);
+  });
+
+*/
